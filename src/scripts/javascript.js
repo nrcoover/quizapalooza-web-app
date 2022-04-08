abefc99bff39d6184d8e0c4f53b58dad32355f5f
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-04-08 11:16:50 -0400

    Tries to Create a Switch statement for Next Button
    This is a draft branch. Was working to create a switch statement to change the wrong answers being checked based on the current active panel. Wanted to dynamically change which wrong answers were being selected to be checked against based on the active panel. Was unable to achieve this and I am temporarily abandoning this approach until a future time when I've done more research. The mess of code is my active trial and error state.

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -4,7 +4,11 @@ const startMenu = document.querySelector('#Start-Menu');
 const startButton = document.querySelector('#Start-Button');
 const nextButton = document.querySelector('#Next-Button');
 const questions = document.getElementsByClassName('question');
+const questionOne = document.querySelector('#Question_One');
+const questionTwo = document.querySelector('#Question_Two');
+const questionThree = document.querySelector('#Question_Three');
 const correctAnswers = document.getElementsByClassName('correct-answer');
+const wrongAnswers = document.getElementsByClassName('wrong-answer');
 const quizFooterElements = document.querySelector('#Quiz-Button-Wrap');
 const startFooterElements = document.querySelector('#Start-Button-Wrap');
 
@@ -27,6 +31,76 @@ function removeActiveQuestionClass() {
     }
 }
 
+// function returns the id of the current active question
+function checkForActiveQuestion() {
+    for (let question of questions) {
+        if (question.classList.contains('active-panel')) {
+            console.log(question.id);
+            return question.id;
+        }
+    }
+}
+
+// function to check if any wrong answer has been selected
+function wrongAnswerChecked() {
+    // let questionNumber = questions.querySelector('.activepanel')
+    // switch (document.querySelector().querySelectorAll('wrong-answer')) {
+        
+        // when a wrong answer is checked, I want to see which panel is active
+        // given the active panel, I want to check if any of the wrong answer of the active panel are checked, but only the answers of the active panel.
+    
+
+
+
+
+        let questionOneForm = document.querySelector('#Question_One > form');
+        let questionOneWrongAnswers = questionOneForm.querySelectorAll('.wrong-answer');
+        let questionTwoForm = document.querySelector('#Question_Two > form');
+        let questionTwoWrongAnswers = questionTwoForm.querySelectorAll('.wrong-answer');
+        let questionThreeForm = document.querySelector('#Question_Two > form');
+        let questionThreeWrongAnswers = questionThreeForm.querySelectorAll('.wrong-answer');
+
+
+
+
+    let currentQuestion = checkForActiveQuestion();
+    // let currentWrongAnswers = document.querySelectorAll(`${currentQuestion} .wrong-answers`);
+
+    // let currentWrongAnswers = currentWrongAnswers.map(wrongAnswers => currentQuestion.classList.contains('active-panel'));
+    // console.log(currentWrongAnswers);
+
+    // console.log(document.querySelectorAll(`${currentQuestion} .wrong-answers`));
+    switch (currentQuestion) {
+        case "Question_One":
+            for (let wrongAnswer of currentWrongAnswers) {
+                
+                console.log(wrongAnswer)
+                if (wrongAnswer.checked) {
+                    console.log(`question one wrong answeres checked`)
+                    return true;
+                }
+            }
+            break;
+        case "Question_Two":
+            for (let wrongAnswer of currentWrongAnswers) {
+                console.log(wrongAnswer)
+                if (wrongAnswer.checked) {
+                    console.log(`question two wrong answers checked`)
+                    return true;
+                }
+            }
+            break;
+    }
+    
+    // for (let wrongAnswer of wrongAnswers) {
+    //     console.log(wrongAnswer)
+    //     if (wrongAnswer.checked) {
+    //         console.log(`The wrong answer is ${wrongAnswer}`)
+    //         return true;
+    //     }
+    // }
+}
+
 // Next Button Moves Through Questions when Correct Answer is Selected {
 let nextButtonClickedCount = null;
 let correctAnswerCounter = 0;
@@ -37,10 +111,11 @@ nextButton.addEventListener('click', function() {
         correctAnswerCounter++;
         removeActiveQuestionClass();
         questions[(nextButtonClickedCount)].classList.add('active-panel');
-    } else if (wrongAnswer[0].checked) {
+        return correctAnswers[correctAnswerCounter-1].checked = false;
+    } else if (wrongAnswerChecked()) {
+        // wrongAnswerChecked();
         console.log("WRONG ANSWER")
     }
-    return correctAnswers[correctAnswerCounter-1].checked = false;
     // This clears the checkmark on the correct answer of the previous question; this is necessary because although the question is hidden from view, it is still existing in the DOM, and so needs to be cleared in order to allow the Next Button to rely on the next question's correct answer to function.
 });
 
